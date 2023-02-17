16d0841d6514369ee61a8a424376b2326e06fead
Thanhntmany <thanhntmany@thanhntmany-laptop>
2023-02-17 11:34:59 +0700

    smt

--- install-alias.sh
+++ install-alias.sh
@@ -1,1 +1,6 @@
-404: Not Found
+#!/bin/bash
+
+
+SCRIPT_DIR=$( dirname -- "$(readlink -f "$0" )")
+export DAS_PKG_DIR="$SCRIPT_DIR"
+alias das='node "$DAS_PKG_DIR/cli"'
--- remoteRun.sh
+++ remoteRun.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-set -e
-
 DAS_GIT_REPO_RAW="https://raw.githubusercontent.com/thanhntmany/directory-as-set-js/main/"
-TEMPDIR=$(mktemp -d -t directory-as-set.js.XXXXXX)
 
+cd $(mktemp -d -t directory-as-set.js.XXXXXX)
+pwd
 curl "$DAS_GIT_REPO_RAW\das.js" --output das.js
 curl "$DAS_GIT_REPO_RAW\install-alias.sh" --output install-alias.sh
 source ./install-alias.sh
