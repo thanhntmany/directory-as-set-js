fc8dfabb17b2d28dcce65223e857df14ceee9114
Thanhntmany <thanhntmany@thanhntmany-laptop>
2023-02-26 15:31:17 +0700

    done inplement using pure Array

--- das.js
+++ das.js
@@ -22,18 +22,19 @@ const fs = require('fs');
 /**
  * File system IO Handler (helper)
  */
-var _join = path.join;
-var _relative = path.relative;
-var _dirname = path.dirname;
-var _resolve = path.resolve;
-var _lstatSync = fs.lstatSync;
-var _mkdirSync = fs.mkdirSync;
-var _rmdirSync = fs.rmdirSync;
-var _readdirSync = fs.readdirSync;
-var _readFileSync = fs.readFileSync;
-var _writeFileSync = fs.writeFileSync;
-var _existsSync = fs.existsSync;
-var _isDirectory = function (dirPath) { return _lstatSync(dirPath).isDirectory() };
+var _join = path.join,
+  _isAbsolute = path.isAbsolute,
+  _relative = path.relative,
+  _dirname = path.dirname,
+  _resolve = path.resolve,
+  _lstatSync = fs.lstatSync,
+  _mkdirSync = fs.mkdirSync,
+  _rmdirSync = fs.rmdirSync,
+  _readdirSync = fs.readdirSync,
+  _readFileSync = fs.readFileSync,
+  _writeFileSync = fs.writeFileSync,
+  _existsSync = fs.existsSync,
+  _isDirectory = function (dirPath) { return _lstatSync(dirPath).isDirectory() };
 
 
 function _tree(dirPath) {
@@ -45,19 +46,19 @@ function _tree(dirPath) {
   return out;
 };
 
-function _treeIntersec(baseDir, partnerDir) {
+function _treeIntersect(baseDir, partnerDir) {
   var out = [], baseCurPath, partnerCurPath;
   _readdirSync(baseDir, { withFileTypes: true }).forEach(function (dirent) {
     if (_existsSync(partnerCurPath = _join(partnerDir, dirent.name))) {
       out.push(baseCurPath = _join(baseDir, dirent.name));
       if (dirent.isDirectory() && _isDirectory(partnerCurPath))
-        out = out.concat(_treeIntersec(baseCurPath, partnerCurPath));
+        out = out.concat(_treeIntersect(baseCurPath, partnerCurPath));
     }
   });
   return out;
 };
 
-function _treeMinus(baseDir, partnerDir) {
+function _treeExcept(baseDir, partnerDir) {
   var out = [], baseCurPath, partnerCurPath;
   _readdirSync(baseDir, { withFileTypes: true }).forEach(function (dirent) {
     if (!_existsSync(partnerCurPath = _join(partnerDir, dirent.name))) {
@@ -66,7 +67,7 @@ function _treeMinus(baseDir, partnerDir) {
     }
     else {
       if (!_isDirectory(partnerCurPath)) return;
-      out = out.concat(_treeMinus(_join(baseDir, dirent.name), partnerCurPath));
+      out = out.concat(_treeExcept(_join(baseDir, dirent.name), partnerCurPath));
     };
   });
   return out;
@@ -107,51 +108,62 @@ function _dirContains(dir, toPath) {
   return relative === "" || relative && !relative.startsWith('..') && !path.isAbsolute(relative);
 };
 
-  /**
-   * Arrary as set Helper
-   */
-  // optimized
-  const ArrayAsSetHelper = {
+/**
+ * Arrary as set Helper
+ */
+// optimized
+const ArrayAsSetHelper = {
 
-    select: function (arr, value) {
-      if (!arr.includes(value)) arr.push(value)
-      return arr;
-    },
+  select: function (arr, value) {
+    if (!arr.includes(value)) arr.push(value)
+    return arr;
+  },
 
-    deselect: function (arr, value) {
-      var idx;
-      if ((idx = arr.indexOf(value)) >= 0) arr.splice(idx, 1);
-      return arr;
-    },
+  deselect: function (arr, value) {
+    var idx;
+    if ((idx = arr.indexOf(value)) >= 0) arr.splice(idx, 1);
+    return arr;
+  },
 
-    distinct: function (arr) {
-      var out = [], listed = {}, l = 0, value;
-      for (value of arr) if (listed[value] !== 1) listed[out[l++] = value] = 1;
+  distinct: function (arr) {
+    var out = [], listed = {}, l = 0, value;
+    for (value of arr) if (listed[value] !== 1) listed[out[l++] = value] = 1;
 
-      return out;
-    },
+    return out;
+  },
 
-    union: function () {
-      return this.distinct(Array.from(arguments).flat(1))
-    },
+  union: function () {
+    return this.distinct(Array.from(arguments).flat())
+  },
 
-    intersect: function (arr1, arr2) {
-      var out = [], l = 0, A, B, value;
-      if (arr1.length < arr2.length) { A = arr1; B = arr2 }
-      else { A = arr2; B = arr1 };
+  intersect: function (arr1, arr2) {
+    var out = [], l = 0, A, B, value;
+    if (arr1.length < arr2.length) { A = arr1; B = arr2 }
+    else { A = arr2; B = arr1 };
 
-      for (value of A) if (B.includes(value)) out[l++] = value;
-      return out;
-    },
+    for (value of A) if (B.includes(value)) out[l++] = value;
+    return out;
+  },
 
-    except: function (arrB, arrP) {
-      var out = [], l = 0, value;
-      for (value of arrB) if (!arrP.includes(value)) out[l++] = value;
-      return out;
-    },
+  except: function (arrB, arrP) {
+    var out = [], l = 0, value;
+    for (value of arrB) if (!arrP.includes(value)) out[l++] = value;
+    return out;
+  },
 
-  };
-  const AAS = ArrayAsSetHelper;
+  //#TODO: have not tested yet
+  keepMatchRegex: function (arr, pattern, flags) {
+    return arr.filter((new RegExp(pattern, flags || "g")).test)
+  },
+
+  //#TODO: have not tested yet
+  removeMatchRegex: function (arr, pattern, flags) {
+    var test = (new RegExp(pattern, flags || "g")).test
+    return arr.filter(function (el) { return !test(el) });
+  },
+
+};
+const AAS = ArrayAsSetHelper;
 
 
 /**
@@ -183,6 +195,28 @@ DASdirectory_proto.treeDir = function (relativePath) {
   return _treeInDir(this.path, relativePath);
 };
 
+DASdirectory_proto.treeDirIntersect = function (partner, relativePath) {
+  if (relativePath === undefined) relativePath = ".";
+  return _relativeMass(
+    this.path,
+    _treeIntersect(
+      _join(this.path, relativePath),
+      _join(partner.path, relativePath),
+    )
+  );
+};
+
+DASdirectory_proto.treeDirExcept = function (partner, relativePath) {
+  if (relativePath === undefined) relativePath = ".";
+  return _relativeMass(
+    this.path,
+    _treeExcept(
+      _join(this.path, relativePath),
+      _join(partner.path, relativePath),
+    )
+  );
+};
+
 /**
  * DASApp
  */
@@ -192,7 +226,7 @@ function DASApp(data) {
   this.isDryrun = data.isDryrun || false;
   this.anchorDir = data.anchorDir || undefined;
 
-  this.selectedSet = Array.from(data.selectedSet) || [];
+  this.selectedSet = data.selectedSet || [];
   this.stashSet = data.stashSet || {};
 
   this.alias = data.alias || {};
@@ -208,7 +242,7 @@ DASApp_proto.toJSON = function () {
     isDryrun: this.isDryrun,
     anchorDir: this.anchorDir,
 
-    selectedSet: this.selectedSet.toJSON(),
+    selectedSet: this.selectedSet,
     stashSet: this.stashSet,
 
     alias: this.alias,
@@ -233,8 +267,8 @@ DASApp_proto.getStateFilePath = function (anchorDir) {
   return _join(anchorDir || this.anchorDir, this.ANCHOR, this.STATEFILE);
 };
 
-DASApp_proto.init = function () {
-  this.anchorDir = process.cwd();
+DASApp_proto.init = function (anchorDir) {
+  this.anchorDir = anchorDir || process.cwd();
   var stateFile = this.getStateFilePath(this.anchorDir);
   _mkdirSync(_dirname(stateFile), { recursive: true });
   return this;
@@ -254,49 +288,57 @@ DASApp_proto.loadState = function (anchorDir) {
   var stateFile = this.getStateFilePath(anchorDir);
   if (!_existsSync(stateFile)) return;
 
+  anchorDir = _dirname(_dirname(stateFile));
+
   var data = JSON.parse(_readFileSync(stateFile, 'utf8'));
-
-  data.alias = Object.fromEntries(Object.entries(data.alias).map(function (alia) {
-    alia[1] = _join(anchorDir, alia[1]);
-    return alia;
-  }));
-
   data.anchorDir = anchorDir;
+
+  // #TODO: make sure every case work properly.
+  var _alias = data.alias, alia;
+
+  for (alia in _alias) _alias[alia] = _join(anchorDir, _alias[alia]);
   data.base = _join(anchorDir, data.base);
   data.partner = _join(anchorDir, data.partner);
 
   var cwd = process.cwd();
-
   if (!_dirContains(data.base, cwd)) {
-    var curBasePath = Object.values(data.alias).find(function (dirPath) { return _dirContains(dirPath, cwd) });
-    if (curBasePath) {
-      if (curBasePath == data.partner) data.partner = data.base;
-      data.base = curBasePath;
+    var curBasePath = data.base;
+
+    if (_dirContains(data.partner, cwd)) {
+      data.base = data.partner;
+      data.partner = curBasePath;
+    }
+    else {
+      for (alia in _alias) if (_dirContains((curBasePath = _alias[alia]), cwd)) {
+        data.base = curBasePath;
+        break;
+      };
     };
   };
 
   this.constructor.call(this, data);
-
   this.relativePath = _relative(this.base.path, cwd);
 };
 
 
 DASApp_proto.saveState = function (anchorDir) {
   if (anchorDir !== undefined) this.anchorDir = anchorDir;
-  if (!this.anchorDir) return;
 
-  var stateFile = this.getStateFilePath();
+  var _anchorDir = this.anchorDir;
+  var stateFile = this.getStateFilePath(_anchorDir);
+  if (!_existsSync(_dirname(stateFile))) return;
+
 
   var data = this.toJSON();
-  data.base = _relative(data.anchorDir, data.base);
-  data.partner = _relative(data.anchorDir, data.partner);
+  data.base = _relative(_anchorDir, data.base);
+  data.partner = _relative(_anchorDir, data.partner);
 
-  for (var ali in data.alias) {
-    data.alias[ali] = _relative(data.anchorDir, data.alias[ali]);
-  };
+  var _alias = data.alias, alia;
+  for (alia in _alias) _alias[alia] = _relative(_anchorDir, _alias[alia]);
 
   delete data.anchorDir;
-  if (_existsSync(_dirname(stateFile))) _writeFileSync(stateFile, JSON.stringify(data, null, 4));
+  _mkdirSync(_dirname(stateFile), { recursive: true });
+  _writeFileSync(stateFile, JSON.stringify(data, null, 4));
 };
 
 DASApp_proto.showState = function () {
@@ -316,29 +358,31 @@ DASApp_proto.showState = function () {
   out += "\n";
 
   if (this.relativePath === null) return out;
-  var selectedArray = this.selectedSet.toArray();
-  var cwd = _join(this.base.path, this.relativePath);
 
-  var selectedInCurDirArray = selectedArray
-    .map(function (rPath) { return _join(this.base.path, rPath) }, this)
-    .filter(function (rPath) { return FSHandler.ifDirContains(cwd, rPath) });
+  var _relativePath = this.relativePath;
+  var cwd = process.cwd();
+  var selectedArray = this.selectedSet;
+  var selectedInCurDirArray = selectedArray.filter(function (rPath) {
+    return _dirContains(cwd, _join(this.base.path, rPath))
+  }, this)
 
   out += "Selected  : " + selectedArray.length + "\n";
   out += "[ " + (selectedArray.length - selectedInCurDirArray.length) + " ] : ...\n";
-  out += "[ " + selectedInCurDirArray.length + " ] : " + this.relativePath + path.sep + "\n";
+  out += "[ " + selectedInCurDirArray.length + " ] : " + _relativePath + path.sep + "\n";
 
-  out += selectedInCurDirArray
-    .map(function (rPath) { return _relative(cwd, rPath) })
-    .map(function (p) { return "  - " + p })
+  var baseOwnSection = this.getBaseOwnSection(_relativePath);
+  var partnerOwnSection = this.getPartnerOwnSection(_relativePath);
+  var lsSet = AAS.union(selectedInCurDirArray, baseOwnSection, partnerOwnSection);
+
+  out += lsSet.sort()
+    .map(function (rPath) {
+      return ""
+        + (selectedInCurDirArray.includes(rPath) ? "*" : " ") + " "
+        + (baseOwnSection.includes(rPath)        ? "*" : " ") + " "
+        + (partnerOwnSection.includes(rPath)     ? "*" : " ") + " "
+        + _relative(_relativePath, rPath)
+    })
     .join("\n");
-
-  var baseOwnSection = this.getBaseOwnSection(this.relativePath);
-  var partnerOwnSection = this.getPartnerOwnSection(this.relativePath);
-  var lsSet = new RPSet(this.selectedSet).filterInRPath(this.relativePath)
-    .join(baseOwnSection)
-    .join(partnerOwnSection);
-
-  console.log(lsSet);
 
   return out;
 };
@@ -384,9 +428,8 @@ DASApp_proto.realia = function (inputString) {
 DASApp_proto.getAliasOf = function (targetPath) {
   if (targetPath === undefined) targetPath = process.cwd();
 
-  for (var alia in this.alias)
-    if (_dirContains(this.alias[alia], targetPath)) return alia;
-
+  var _alias = this.alias, alia;
+  for (alia in _alias) if (_dirContains(_alias[alia], targetPath)) return alia;
   return "";
 };
 
@@ -404,34 +447,31 @@ DASApp_proto.getPartnerOwnSection = function (relativePath) {
 };
 
 DASApp_proto.getBaseSection = function (relativePath) {
-  return this.base.treeDir(relativePath || this.relativePath)
-    .filter(this.partner.treeDir(relativePath || this.relativePath));
+  return this.base.treeDirExcept(this.partner, relativePath || this.relativePath);
 };
 
 DASApp_proto.getInterSection = function (relativePath) {
-  return this.base
-    .inter(this.partner.path, relativePath || this.relativePath);
+  return this.base.treeDirIntersect(this.partner, relativePath || this.relativePath);
 };
 
 DASApp_proto.getPartnerSection = function (relativePath) {
-  return this.partner.treeDir(relativePath || this.relativePath)
-    .filter(this.base.treeDir(relativePath || this.relativePath));
+  return this.partner.treeDirExcept(this.base, relativePath || this.relativePath);
 };
 
 // Selection
-DASApp_proto.select = function () {
-  var rPaths = [];
-  for (var i = 0, l = arguments.length; i < l; i++) {
-    rPaths.push(_join(this.relativePath, arguments[i]))
-  };
+DASApp_proto.normalizePath = function (filePath) {
+  return _isAbsolute(filePath)
+    ? _relative(_join(this.base.path, this.relativePath), filePath)
+    : _join(this.relativePath, filePath);
+};
 
-  var _set = this.selectedSet;
-  return _set.select.apply(_set, rPaths);
+DASApp_proto.select = function () {
+  return this.selectedSet = AAS.union(this.selectedSet, Array.from(arguments).map(this.normalizePath, this))
 };
 DASApp_proto.select.expectedLength = -1;
 
 DASApp_proto.selectSet = function (rpSet) {
-  return this.selectedSet.join(rpSet)
+  return this.selectedSet = AAS.union(this.selectedSet, rpSet)
 };
 
 DASApp_proto.selectBase = function () {
@@ -446,24 +486,13 @@ DASApp_proto.selectPartner = function () {
   return this.selectSet(this.getPartnerSection());
 };
 
-DASApp_proto.selectRegex = function (pattern, flags) {
-  console.log("pattern:", pattern);
-  return this.selectedSet.filterMatchRegex(pattern, flags);
-};
-
 DASApp_proto.deselect = function () {
-  var rPaths = [];
-  for (var i = 0, l = arguments.length; i < l; i++) {
-    rPaths.push(_join(this.relativePath, arguments[i]))
-  };
-
-  var _set = this.selectedSet;
-  return _set.deselect.apply(_set, rPaths);
+  return this.selectedSet = AAS.except(this.selectedSet, Array.from(arguments).map(this.normalizePath, this))
 };
 DASApp_proto.deselect.expectedLength = -1;
 
 DASApp_proto.deselectSet = function (rpSet) {
-  return this.selectedSet.filter(rpSet)
+  return this.selectedSet = AAS.except(this.selectedSet, rpSet)
 };
 
 DASApp_proto.deselectBase = function () {
@@ -478,21 +507,27 @@ DASApp_proto.deselectPartner = function () {
   return this.deselectSet(this.getPartnerSection());
 };
 
-DASApp_proto.deselectRegex = function (pattern, flags) {
-  return this.selectedSet.filterNotMatchRegex(pattern, flags);
-};
-
 DASApp_proto.getSelectedSet = function () {
   return this.selectedSet;
 };
 
+//#TODO: have not tested yet
+DASApp_proto.keepMatchRegex = function (pattern, flags) {
+  return this.selectedSet = AAS.keepMatchRegex(this.selectedSet, pattern, flags);
+};
+
+//#TODO: have not tested yet
+DASApp_proto.removeMatchRegex = function (pattern, flags) {
+  return this.selectedSet = AAS.removeMatchRegex(this.selectedSet, pattern, flags);
+};
+
 DASApp_proto.clearSelectedSet = function () {
-  return this.selectedSet.clear();
+  return this.selectedSet = [];
 };
 
 DASApp_proto.stashSelectedSet = function (key) {
   if (key === undefined) key = Object.keys(this.stashSet).length;
-  this.stashSet[key] = Object.assign({}, this.selectedSet);
+  this.stashSet[key] = this.selectedSet.slice();
   return key
 };
 
@@ -599,7 +634,6 @@ DASCmdRunner_proto.cmdAlias = {
   "sio": "selectInterOlder",
   "sin": "selectInterNewer",
   "sp": "selectPartner",
-  "sr": "selectRegex",
 
   "d": "deselect",
   "d-": "deselect",
@@ -608,8 +642,9 @@ DASCmdRunner_proto.cmdAlias = {
   "dio": "deselectInterOlder",
   "din": "deselectInterNewer",
   "dp": "deselectPartner",
-  "dr": "deselectRegex",
 
+  "kmr": "keepMatchRegex",
+  "rmr": "removeMatchRegex",
   "ls": "getSelectedSet",
   "cls": "clearSelectedSet",
 
@@ -732,7 +767,6 @@ exports = module.exports = DASApp;
 exports.createApp = createApp;
 exports.DASApp = DASApp;
 exports.DASdirectory = DASdirectory;
-exports.FSHandler = FSHandler;
 
 
 /**
@@ -750,11 +784,13 @@ if (require.main === module || require.main === undefined || require.main.id ===
   console.log(
     out
       ? (
-        out.toString
-          ? out.toString()
-          : out.toJSON
-            ? out.toJSON()
-            : out
+        Array.isArray(out)
+          ? out.sort().join("\n")
+          : out.toString
+            ? out.toString()
+            : out.toJSON
+              ? out.toJSON()
+              : out
       )
       : out
   );
